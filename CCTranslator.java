94d84d04743ea55ce13dfc470e70ef50216b4caf
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-10-09 20:08:42 +0300

    Must test
    Must test

--- CCTranslator.java
+++ CCTranslator.java
@@ -1,1 +1,49 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Enumeration;
+import java.util.Properties;
 
+public class CCTranslator {
+
+	private Properties greek , eng;
+	public boolean english;
+	
+	public CCTranslator() throws Exception{
+		
+		english = true;
+		
+		greek = new Properties();
+		eng = new Properties();
+		
+		greek.load(new FileInputStream("res" + File.separator + "greek.properties"));
+		eng.load(new FileInputStream("res" + File.separator + "eng.properties"));
+		
+	}
+	
+	public String doIt(String phrase , boolean encrypt){
+		
+		Properties p;
+		
+		if(english){
+			p = eng;
+		}else{
+			p = greek;
+		}
+		
+		String s = phrase;
+		
+		Enumeration e = p.propertyNames();
+		
+		while(e.hasMoreElements()){
+			String key = (String) e.nextElement();
+			String value = p.getProperty(key);
+			if(encrypt){
+				s = s.replaceAll(key, value);
+			}else{
+				s = s.replaceAll(value , key);
+			}
+		}
+		return s;
+	}
+	
+}
